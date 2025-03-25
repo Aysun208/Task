85b8eefaeff9142125922c4aaa1282a2050f99e6
Aysun208 <aysunmm-bb208@code.edu.az>
2025-03-25 16:59:49 +0400

    added

---
+++ BankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class BankAccount
+    {
+        private decimal _balance;
+        public decimal Balance { get; }
+        private int _AccountNumber;
+        public int AccountNumber
+        {
+            get
+            { return _AccountNumber; }
+        }
+        public string Ownername { get; set; }
+
+        public BankAccount(int AccountNumber, string customername, int balance)
+        {
+            this._AccountNumber = AccountNumber;
+            this.Ownername = customername;
+            this.Balance = balance;
+
+        }
+        public void Deposit(decimal depositamount)
+        {
+            if (depositamount > 0)
+            {
+                _balance += depositamount;
+                Console.WriteLine($"{depositamount} meblege elave olundu ");
+            }
+            else
+            {
+                Console.WriteLine(" mebleg menfi ola bilmez");
+            }
+        }
+        public void WithDraw(decimal withdrawamount)
+        {
+            if (_balance > withdrawamount)
+            {
+                _balance -= withdrawamount;
+                Console.WriteLine($"{withdrawamount} balansdan cixarildi");
+            }
+        }
+        public void DisplayAccountInfo()
+        {
+            Console.WriteLine($"balansiniz {_balance} , adiniz {Ownername} , nomreniz {AccountNumber}");
+        }
+        public void TransferFunds(BankAccount recipient, decimal amount)
+        {
+            recipient.Deposit(amount);
+            Console.WriteLine($"{recipient}e {amount} azn kocuruldu");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(245, "asd", 245);
+            BankAccount account1 = new BankAccount(245, "asd", 245);
+            Console.WriteLine("emeliyyat secin");
+            Console.WriteLine(" \n 1.DisplayAccountInfo \n 2. Deposit \n 3. WithDraw \n 4. esas menyu \n 0.Quit Application");
+            int Menu = int.Parse(Console.ReadLine());
+       
+            switch(Menu)
+            {
+                case 1:
+                    account1.DisplayAccountInfo();
+                    break;
+                    case 2:
+                    Console.WriteLine("ne qeder pul elave etmek isteyirsiniz");
+                    decimal deposit = int.Parse(Console.ReadLine());
+                    account1.Deposit(deposit);
+                    break;
+                    case 3:
+                    Console.WriteLine("ne qeder pul elave etmek isteyirsiniz");
+                    decimal withdraw = int.Parse(Console.ReadLine());  
+                    account1.   WithDraw(withdraw);
+                    break;
+                    case 4:
+                    Console.WriteLine("esas menyuya qayit");
+                    break;
+                case 0:
+                    Console.WriteLine("bitir");
+                    return;
+            }
+        }
+
+
+    }
+}
